c0d7ddcca6970c297659bab63e0399bcc5b2aa66
Greg1377 <liardgregory@gmail.com>
2022-07-25 20:29:52 +0200

    etape1

--- front/js/product.js
+++ front/js/product.js
@@ -1,2 +1,9 @@
 //Récupération de l'id via les paramètres de l'url 
-const idProduct = new URL (window.location.href).searchParams.get("id")
+const idProduct = new URL (window.location.href).searchParams.get("id");
+console.log("j'ai récupérer l'id suivant: " + idProduct);
+
+//récupération des données de l'API 
+async function getProducts(){
+    let getProducts = await fecht('http://localhost:3000/api/products');
+    return products.json();
+};
